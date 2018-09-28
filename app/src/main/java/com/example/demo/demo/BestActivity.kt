7cc17ae16ba2dc539bedd0b543434ef54588a94a
wulinjun <wulinjun@chelun.com>
2018-09-28 15:51:20 +0800

    test git update

---
+++ app/src/main/java/com/example/demo/demo/BestActivity.kt
@@ -0,0 +1,26 @@
+package com.example.demo.demo
+
+import android.app.Activity
+import android.os.Bundle
+import android.webkit.JsPromptResult
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class BestActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        myWeb.webChromeClient = object : WebChromeClient() {
+            override fun onJsPrompt(view: WebView, url: String, message: String, defaultValue: String, result: JsPromptResult): Boolean {
+                result.confirm()
+                return super.onJsPrompt(view, url, message, defaultValue, result)
+            }
+        }
+
+    }
+
+
+}
